ef09c5255b9cccd868d5572f2cebd4f71e0a9d5f
Kwame Botse-Baidoo <kwamebb36@gmail.com>
2026-02-15 21:45:23 -0500

    completed user story 4

--- CourseServices.cs
+++ CourseServices.cs
@@ -58,7 +58,16 @@ namespace cs330_proj1
 
         /* As a student I want to see all course offerings by semester and department so that I can 
         choose major courses to register for */
-
+         public List<CourseOffering> getCourseOfferingsBySemesterAndDept(String semester, String dept) {
+            List<CourseOffering> result = new List<CourseOffering>();
+            foreach(CourseOffering co in repo.Offerings) {
+                if(co.Semester.Equals(semester) 
+                    && co.TheCourse.Name.StartsWith(dept)) {
+                    result.Add(co);
+                }
+            }
+            return result;
+         }
         /* As a student I want to see all courses that meet a core goal, so that I can plan out
            my courses over the next few semesters and choose core courses that make sense for me */
 
--- Program.cs
+++ Program.cs
@@ -33,14 +33,14 @@ namespace cs330courses
                 Console.WriteLine(c);
             }
             Console.WriteLine("---------");
-            /*
+            
             List<CourseOffering> theList4 = service.getCourseOfferingsBySemesterAndDept("Fall 2020","CSCI");
             foreach(CourseOffering c in theList4) {
                 Console.WriteLine(c);
             }
             Console.WriteLine("---------");
 
-            
+            /*
             List<Course> theList5 = service.getCoursesByGoalId("CG2");
             foreach(Course c in theList5) {
                 Console.WriteLine(c);
